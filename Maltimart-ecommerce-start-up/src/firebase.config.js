ff958eba841718b98644ab53265d3ca166f30cf4
yuvalleberstein21 <94398909+yuvalleberstein21@users.noreply.github.com>
2023-06-17 12:20:51 +0300

    17/06

---
+++ Maltimart-ecommerce-start-up/src/firebase.config.js
@@ -0,0 +1,24 @@
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage';
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDj21fyMitkV1EoaCOSXeBGbU0u6XYRH5c",
+    authDomain: "maltimart-685af.firebaseapp.com",
+    projectId: "maltimart-685af",
+    storageBucket: "maltimart-685af.appspot.com",
+    messagingSenderId: "292796469305",
+    appId: "1:292796469305:web:c3e450a0d4588bfd00bde8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+export default app
