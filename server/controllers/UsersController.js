1088862a57c95bd09bb82888545129a157c26acf
yuvalleberstein21 <94398909+yuvalleberstein21@users.noreply.github.com>
2023-06-19 14:35:30 +0300

    database

--- server/controllers/UsersController.js
+++ server/controllers/UsersController.js
@@ -1,16 +1,44 @@
 const con = require('../DB/database');
-
-
-
+const bcrypt = require('bcryptjs');
 
 exports.register = (req, res) => {
-    res.send('work')
+    const { username, email, password } = req.body;
+    const image = req.file.filename;
+
+    con.query("SELECT email FROM users WHERE email =?", [email], async (err, result) => {
+        if (err) return res.json(err)
+        if (result.length > 0) return res.send({ message: "User already exists" });
+
+        let hashedPassword = await bcrypt.hash(password, 8);
+        con.query("INSERT INTO users (username,email,password,image) VALUES (?,?,?,?)", [username, email, hashedPassword, image],
+            (err, result) => {
+                if (err) return res.json(err);
+                const userId = result.insertId;
+                return res.status(200).send({ userId });
+            })
+    })
 }
 
 
 exports.login = async (req, res) => {
-    res.send('work')
+    const { email, password } = req.body;
 
+    con.query("SELECT * FROM users WHERE email =?", email, (err, result) => {
+        if (err) {
+            res.send({ err: err });
+        }
+        if (result.length > 0) {
+            bcrypt.compare(password, result[0].password, (error, response) => {
+                if (response) {
+                    res.send(result);
+                } else {
+                    res.send({ message: "Wrong email/password combination !" });
+                }
+            })
+        } else {
+            res.send({ message: "User dosen't exist" });
+        }
+    });
 }
 
 
--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const multer = require('multer');
 const router = express.Router();
 
 
